1684f435e70c445556c8f61c4933377a7cf0687e
Aryan <aryans@tamu.edu>
2018-11-30 02:30:04 -0600

    Added requests module

--- crawler.py
+++ crawler.py
@@ -4,27 +4,32 @@ import html2text
 import pprint
 import os
 import shutil
+import requests
 
-def save_page_text(page):
-    html = get_page(page)
-    if (html is not None and page[-4:] is not ".pdf"):
-        try:
-            text = html2text.html2text(html)
-        except:
-            print "ERROR in html to text parser for page: ", page
-            return
-        raw_list = text.splitlines()
+def save_page_text(url):
+    try:
+        page_response = requests.get(url, timeout=5, headers=headers)
+    except:
+        print "ERROR is saving text of url: ", url
+        return
+    soup = BeautifulSoup(page_response.content, "html.parser")
+    text = soup.findAll(['p', 'a', 'h1', 'h2'], text=True)
+    if (text is not None):
+        # raw_list = text.splitlines()
         new_list = []
-        for line in raw_list:
+        for line in text:
             line = line.encode('utf-8').strip()
             if line != '':
                 line = line + '\n'
                 new_list.append(line)
         clean_text = "".join(new_list)
-        file_name = page.replace('/','')
+        file_name = url.replace('/','')
         f = open("dump-texts/" + file_name + '.txt', "w")
         f.write(clean_text)
         f.close()
+    else:
+        print "ERROR in html to text parser for url: ", url
+        return
 
 # def get_next_target(page):
 #     start_link = page.find('<a href=')
@@ -50,15 +55,17 @@ def get_page(url):
         return content
     except:
         print "ERROR GETTING THE PAGE: ", url
-        return
+        return None
 
-def get_all_links(page):
+def get_all_links(page_url):
     links=[]
-    # print("*********** Printing links for Page: ",page)        
+    page_response = requests.get(url, timeout=5, headers=headers)
+    soup = BeautifulSoup(page_response.content, "html.parser")
+    # print("*********** Printing links for Page: ",page_url)        
     for link in soup.find_all('a', href=True):
         link = unicode(link.get('href'))
         # if link.find("http://www.")== -1 or link.find("http://") == -1:
-        #     link = page + '/' + link
+        #     link = page_url + '/' + link
         # print(link)
         links.append(link)
     # print("*********** End Printing links")
@@ -69,33 +76,38 @@ def union(p,q):
         if e not in p:
             p.append(e)
 
-def crawl_web(seed):
-    tocrawl = [seed]
+def crawl_web(seed_url):
+    tocrawl = [seed_url]
     crawled = set()
     graph = {}
     while (tocrawl and len(crawled) <= pageslimit):
-        f_crawled = open("crawled.txt","a+")
-        page = tocrawl[0]
+        f_crawled = open("html-crawled.txt", "a+")
+        page_url = tocrawl[0]
         tocrawl = tocrawl[1:]
-        if page not in crawled:
-            outlinks = get_all_links(page)
-            crawled.add(page)
-            f_crawled.write(page+"\n")
+        if page_url not in crawled:
+            outlinks = get_all_links(page_url)
+            crawled.add(page_url)
+            f_crawled.write(page_url + "\n")
             f_crawled.close()
             if (len(outlinks) == 0):
                 continue
             union(tocrawl, outlinks)
-            graph[page] = outlinks
-            save_page_text(page)
-        print("************* Graph size = ",len(graph))
+            graph[page_url] = outlinks
+            save_page_text(page_url)
+        print "************* Graph size =",len(graph)
     return graph
 
-page = "http://courses.cse.tamu.edu/caverlee/csce670/schedule.html"
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
+# url = "https://www.amazon.com/"
+url = "http://courses.cse.tamu.edu/caverlee/csce670/index.html"
 pageslimit = 20
+if os.path.exists("html-crawled.txt"):
+    os.remove("html-crawled.txt")
 if os.path.exists("dump-texts"):
     shutil.rmtree("./dump-texts")
 os.makedirs("dump-texts")
-content = get_page(page)
-soup = BeautifulSoup(content, 'html.parser')
-graph = crawl_web(page)
+# page_response = requests.get(url, timeout=5, headers=headers)
+# soup = BeautifulSoup(page_response.content, "html.parser")
+# data = soup.findAll(text=True)
+graph = crawl_web(url)
 # pprint.pprint(graph)
