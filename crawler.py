ddfec106efa0f39f464a985cffe9a06556c00094
Aryan <aryans@tamu.edu>
2018-11-30 03:02:34 -0600

    Finished the crawler

--- crawler.py
+++ crawler.py
@@ -1,61 +1,25 @@
 from bs4 import BeautifulSoup
-import urllib2
-import html2text
 import pprint
 import os
 import shutil
 import requests
+import re
 
 def save_page_text(url):
     try:
         page_response = requests.get(url, timeout=5, headers=headers)
     except:
-        print "ERROR is saving text of url: ", url
+        print "ERROR in saving text of url: ", url
         return
     soup = BeautifulSoup(page_response.content, "html.parser")
-    text = soup.findAll(['p', 'a', 'h1', 'h2'], text=True)
-    if (text is not None):
-        # raw_list = text.splitlines()
-        new_list = []
-        for line in text:
-            line = line.encode('utf-8').strip()
-            if line != '':
-                line = line + '\n'
-                new_list.append(line)
-        clean_text = "".join(new_list)
+    for node in soup.findAll(['p', 'a', 'h1', 'h2']):
+        line = ''.join(node.findAll(text=True))
+        line = line.strip()
+        line = line.encode('utf-8')
         file_name = url.replace('/','')
-        f = open("dump-texts/" + file_name + '.txt', "w")
-        f.write(clean_text)
+        f = open("dump-texts/" + file_name + '.txt', "a+")
+        f.write(line)
         f.close()
-    else:
-        print "ERROR in html to text parser for url: ", url
-        return
-
-# def get_next_target(page):
-#     start_link = page.find('<a href=')
-#     if start_link == -1:
-#         return None,0
-#     start_quote = page.find('"',start_link)
-#     end_quote = page.find('"',start_quote+1)
-#     url = page[start_quote+1:end_quote]
-#     return url,end_quote
-
-# def print_all_links(page):
-#     while True:
-#         url, endpos=get_next_target(page)
-#         if url:
-#             print url
-#             page=page[endpos:]
-#         else:
-#             break
-
-def get_page(url):
-    try:                                
-        content = urllib2.urlopen(url).read()
-        return content
-    except:
-        print "ERROR GETTING THE PAGE: ", url
-        return None
 
 def get_all_links(page_url):
     links=[]
@@ -64,9 +28,8 @@ def get_all_links(page_url):
     # print("*********** Printing links for Page: ",page_url)        
     for link in soup.find_all('a', href=True):
         link = unicode(link.get('href'))
-        # if link.find("http://www.")== -1 or link.find("http://") == -1:
-        #     link = page_url + '/' + link
-        # print(link)
+        if not re.match(r'^http', link):
+            link = page_url + '/' + link
         links.append(link)
     # print("*********** End Printing links")
     return links
@@ -106,8 +69,5 @@ if os.path.exists("html-crawled.txt"):
 if os.path.exists("dump-texts"):
     shutil.rmtree("./dump-texts")
 os.makedirs("dump-texts")
-# page_response = requests.get(url, timeout=5, headers=headers)
-# soup = BeautifulSoup(page_response.content, "html.parser")
-# data = soup.findAll(text=True)
 graph = crawl_web(url)
 # pprint.pprint(graph)
